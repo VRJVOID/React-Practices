2a038b6548e0fe7ed33c7b0694b2d171a84067fc
V0id <86529756+VRJVOID@users.noreply.github.com>
2024-07-12 17:41:13 +0530

    Update TextForm.js with new changes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,6 +13,36 @@ function TextForm(props) {
     setText(newText1);
   };
 
+  const handleClick2 = () => {
+    console.log("I am clicked", +text);
+    let newText1 ='';
+    let ch;
+    for(let i in text)
+    {
+        ch = text[i];
+        if(ch === 'a' || ch === 'e' || ch === 'i' ||
+            ch === 'o' || ch === 'u')
+            {
+                newText1 = newText1 + ch;
+                }
+                }
+                setText(newText1);
+                };
+
+                
+  const handleClick3 = () => {
+    console.log("I am clicked", +text);
+    let newText1 = '';
+    let ch;
+    for(let i=text.length;i>=0;i--){
+        ch = text.charAt(i);
+        newText1 = newText1 + ch;
+    }
+    setText(newText1);
+  };
+
+
+
   const handleOnChange = (e) => {
     console.log("I am changed");
     setText(e.target.value);
@@ -37,8 +67,18 @@ function TextForm(props) {
           Convert to Uppercase
         </button>
 
-        <button className="btn btn-primary mx-5" onClick={handleClick1}>
+        <button className="btn btn-primary mx-2" onClick={handleClick1}>
           Convert to Lowercase
+        </button>
+
+        <button className="btn btn-primary mx-2" onClick={handleClick2}>
+          Vowel
+        </button>
+    
+
+      
+      <button className="btn btn-primary mx-2" onClick={handleClick3}>
+          Reverse
         </button>
       </div>
 

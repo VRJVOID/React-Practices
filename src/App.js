509ef9e0004efb9a1a3a02bd55d27cfded68681c
V0id <86529756+VRJVOID@users.noreply.github.com>
2024-07-14 14:51:45 +0530

    Updated Navbar with darkMode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+ import TextForm from './components/TextForm';
+ import { useState } from 'react';
+// import About from './components/About';
 function App() {
+  const [mode,setMode] = useState('light')
+  
+  const toggleSwitch = () => {
+    if(mode === 'light'){
+      setMode('dark')
+    }
+    else{
+      setMode('light')  
+    }
+  }
+  
+  
+  
   return (
    <>
 
-<Navbar title="TextUtils"/>
+<Navbar title="TextUtils" mode={mode} toggleSwitch={toggleSwitch} />
 <div className="container my-3">
-<TextForm heading="Enter the heading to continue"/>
+{ <TextForm heading="Enter the heading to continue"/> }
+{/* <About/> */}
 </div>
 
 </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar(props) {
   return (
     <div>
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,6 +19,11 @@ function Navbar(props) {
         </li>
       </ul>
       
+    
+      <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+  <input className="form-check-input" type="checkbox" role="switch" onClick={props.toggleSwitch} id="flexSwitchCheckDefault" />
+  <label className="form-check-label" for="flexSwitchCheckDefault">Enable Dark-Mode</label>
+</div>
     </div>
   </div>
 </nav>
